be58ff84e8c4d700a2d846b51ff418cb8d5d6971
IvanMart2019 <IvanMart001@yandex.ru>
2020-10-09 13:01:18 +0300

    fixed bags

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,7 @@ Vue.use(VueRouter)
     path: '/Article',
     name: 'Article',
     props: true,
-    meta: {layout: 'Empty'},
+    meta: {layout: 'Empty', auth: true},
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
